6c52da3c8b20ea8356d3bc9a1c1fa36f08db5b2e
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-02-03 21:30:34 +0000

    admin can delete users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,6 +8,12 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to movies_path
+  end
+  
   # def new
   #   @user = User.new
   # end
